d19e9aa7319d0168c55c923c540be38eb5fe5a2c
LIBOH <lbh2452366947@outlook.com>
2024-05-17 11:57:40 +0800

    电脑重装备份

--- calculator.py
+++ calculator.py
@@ -3,297 +3,122 @@ from decimal import Decimal, getcontext
 from colorama import init, Fore
 
 from blocks_mro import Block, block_factory
-from utils import blocks_registry, utils_checkIgnore
+from utils import blocks_registry, utils_isIgnore
 
-getcontext().rounding = "ROUND_DOWN"
-custom_exp = getcontext().exp( Decimal( "0" ) )
-
-
-# class Calculator:
-#     STACK_COUNT = Decimal( 64 )
-#
-#     def __init__( self, block: Block, quantity: int ) -> None:
-#         self.block = block
-#         self.quantity = quantity
-#         init()
-#
-#     def __repr__( self ) -> str:
-#         return f"Calculator{{ Block={self.block.block_name}, Quantity={self.quantity}, Total_Recipes={self.d_tr}, Total_To_Recipes={self.d_tts}, Inner_Recipes={self.d_ir} }}"
-#
-#     def __str__( self ) -> str:
-#         return self.__repr__()
-#
-#     def __getChange( self, total, flag=False ) -> Decimal:
-#         """计算材料找零
-#
-#         Args:
-#             total: 从 get_total_recipe 计算得到的所需材料的总数
-#             flag: 表示 self.block.outputQty 的值不等于一. Defaults to False.
-#
-#         Returns:
-#             材料的找零  例如: 制作77个红石粉需要 红石块: (0 x 64 + 8) + {5} 个红石粉, 其中 {} 中的值为通过该方法得到
-#         """
-#
-#         if flag:
-#             return Decimal( self.quantity - total )
-#
-#         return Decimal( self.quantity - total * self.block.outputQty )
-#
-#     def __getTextLength( self, text: str ) -> int:
-#         """计算文本长度
-#
-#         Args:
-#             text: 需要计算长度的文本
-#
-#         Returns:
-#             计算过的最长的文本
-#         """
-#         text_length = len( text )
-#         text_length = max( len( text ), text_length )
-#         return text_length
-#
-#     def __calculateOutput(
-#             self,
-#             total_recipe: dict[str:int],
-#             total_to_stacks: dict[str:str],
-#             inner_recipe: list[dict],
-#             output_color: str,
-#             inner_output_color: str,
-#             beautify: bool,
-#     ) -> None:
-#         """打印材料的计算结果
-#
-#         Args:
-#             total_recipe: 用户所查询的方块的总配方
-#             total_to_stacks: 总配方按组显示
-#             inner_recipe: 总配方中需另外制作的方块的配方
-#             output_color: 用于更改本方法所打印内容的颜色
-#             inner_output_color: 用于更改打印需另外制作的配方的颜色
-#             beautify: 需另外制作的方块的配方是否启用美化输出
-#         """
-#         t_l = self.__getTextLength( str( total_to_stacks ) )
-#         self.d_tr = total_recipe
-#         self.d_tts = total_to_stacks
-#         self.d_ir = inner_recipe
-#
-#         print( f"{output_color if output_color else Fore.RESET}", end="" )
-#         print( total_recipe )
-#         print( total_to_stacks )
-#
-#         output_dividing_line = "-" * (t_l + 8)
-#         is_ignore = utils_checkIgnore( self.block.block_name )
-#
-#         if not is_ignore:
-#             print( output_dividing_line )
-#             if beautify:
-#                 self.__innerOutput( inner_output_color )
-#             else:
-#                 print( inner_recipe )
-#
-#         else:
-#             print( f"{output_dividing_line}\n" )
-#
-#     def __innerOutput( self, output_color=None ) -> None:
-#         """需另外制作的方块的配方
-#
-#         Args:
-#             output_color: 用于更改本方法所打印内容的颜色. Defaults to None.
-#         """
-#         t_len = 0
-#         for result in self.result_list:
-#             t_len = self.__getTextLength( str( result ) ) + 3
-#
-#         inner_output_dividing_line = "-" * t_len
-#         for recipe in self.inner_recipe:
-#             print( f"{output_color if output_color else Fore.RESET}", end="" )
-#             print( f"其中制作 {recipe[1]} 个{recipe[0]}需要:" )
-#             print( recipe[2] )
-#             print( inner_output_dividing_line )
-#
-#         print( "\n" )
-#
-#     def getTotalRecipe( self ) -> dict[str:int]:
-#         """计算用户所查询的方块的总配方
-#
-#         Returns:
-#             包含配方的字典 -> {'红石粉': 90, ...}
-#         """
-#         # 此方法计算 self.block.outputQty != 1 的 block 是不准确的，可能会少不足一组的数量
-#         # 而 get_total_to_stacks 方法获取的是准确的
-#         total_recipe = { }
-#         if self.block.outputQty == 1:
-#             for recipe in self.block.recipes:
-#                 for k, v in recipe.items():
-#                     # @k: 制作 self.block 的配方中的材料名
-#                     # @v: 制作 self.block 的配方中材料所需要的数量
-#                     total_recipe.update( { k: v * self.quantity } )
-#
-#         else:
-#             for recipe in self.block.recipes:
-#                 for k, v in recipe.items():
-#                     # @k: 制作 self.block 的配方中的材料名
-#                     num = Decimal(
-#                         Decimal( self.quantity ) / Decimal( self.block.outputQty )
-#                     ).quantize( custom_exp )
-#                     total = num * v
-#
-#                     total_recipe.update( { k: int( total ) } )
-#
-#         return total_recipe
-#
-#     def getTotalToStacks( self ) -> dict[str:str]:
-#         """计算总配方按组显示
-#
-#         Returns:
-#             包含按组显示的配方的字典 -> {'红石粉': '90 = (1 x 64 + 26)', ...}
-#         """
-#         total_to_stacks = dict()
-#         recipe = self.getTotalRecipe()
-#         for k, v in recipe.items():
-#             # @k: 制作 self.block 的材料名
-#             # @v: 制作 self.block 对应的材料所需要的总数
-#             total = Decimal( v )  # 所需材料的组数
-#             stacks = Decimal( total / self.STACK_COUNT ).quantize( custom_exp )  # 所需材料的组数
-#             amount = Decimal( total - stacks * self.STACK_COUNT ).quantize( custom_exp )  # 所需材料不足一组的找零
-#
-#             # !
-#             print( k, total, stacks, amount, self.block.outputQty )
-#
-#             # 计算找零
-#             change = 0
-#             if self.block.outputQty == 1:
-#                 if (v * self.block.outputQty) < self.quantity:
-#                     change = self.__getChange( v )
-#             else:
-#
-#                 for v2 in self.block.recipes.values():
-#                     total = v / v2 * self.block.outputQty
-#                     if total < self.quantity:
-#                         change = self.__getChange( total, flag=True )
-#
-#                         # !
-#                         # print(change)
-#
-#             text = f"{v} = ({stacks} x {self.STACK_COUNT} + {amount})"
-#             if change != 0 and not utils_checkIgnore( k ):
-#                 text += f" + {change} 个{self.block.block_name}"
-#
-#             total_to_stacks.update( { k: text } )
-#
-#         return total_to_stacks
-#
-#     def getInnerRecipe( self ) -> list[dict[str:str]]:
-#         """计算总配方中需另外制作的方块的配方
-#
-#         Returns:
-#             包含总配方中需另外制作的方块的配方的列表 -> [{'红石粉': '90 = (1 x 64 + 26)'}, {...}, ...]
-#         """
-#         self.inner_recipe = []
-#         self.result_list = []
-#         origin_recipe = self.getTotalRecipe()
-#         for k, v in origin_recipe.items():
-#             # @k: 制作 self.block 的配方中的材料所需要的配方的材料名
-#             # @v: 制作 self.block 的配方中的材料所需要的配方的数量
-#             if k in blocks_registry:
-#                 self.inner_recipe.append( [k, v] )
-#
-#         for index, i_recipe in enumerate( self.inner_recipe ):
-#             inner_block = Block( i_recipe[0] )
-#             inner_calculator = Calculator( inner_block, i_recipe[1] )
-#
-#             result = inner_calculator.getTotalToStacks()
-#             self.result_list.append( result )
-#
-#             for k, v in result.items():
-#                 self.inner_recipe[index].append( f"{k}: {v}" )
-#
-#         return self.result_list
-#
-#     def calculate(
-#             self, /, beautify=False, output_color=None, inner_output_color=None
-#     ) -> None:
-#         """计算用户所查询的方块的配方
-#
-#         Args:
-#             beautify: 是否启用美化输出. Defaults to False.
-#             output_color: 用于更改打印输出的颜色. Defaults to None.
-#             inner_output_color: 用于更改打印输出需另外制作的配方的颜色. Defaults to None.
-#         """
-#         d_total_recipe = self.getTotalRecipe()
-#         d_total_to_stacks = self.getTotalToStacks()
-#         d_inner_recipe = self.getInnerRecipe()
-#         self.__calculateOutput( d_total_recipe, d_total_to_stacks, d_inner_recipe, output_color, inner_output_color,
-#                                 beautify )
-
-
-# def main():
-#     while True:
-#         print( f"{Fore.GREEN}", end="" )
-#         blockName = input( 'Enter block name (Enter "q" to quit): ' )
-#         if blockName == "q":
-#             exit( 0 )
-#         try:
-#             block = Block( blockName )
-#         except ValueError as e:
-#             print( f"{Fore.RED}!请重试输入, {e}" )
-#             continue
-#
-#         blockQuantity = input( "Enter block quantity: " )
-#         if blockQuantity.isdigit():
-#             blockQuantity = int( blockQuantity )
-#         else:
-#             print( f"{Fore.RED}!请输入一个阿拉伯数字!" )
-#             continue
-#
-#         print( "-" * 50 )
-#         calculator = Calculator( block, blockQuantity )
-#         calculator.calculate(
-#             beautify=True, output_color=Fore.CYAN, inner_output_color=Fore.YELLOW
-#         )
+getcontext().rounding = "ROUND_HALF_UP"
+cus_exp = getcontext().exp(Decimal("0"))
 
 
 class Calculator:
-    def __init__( self, block: Block, qty: int ):
+    def __init__(self, block: Block, qty: int):
         self.block = block
         self.qty = qty
+        init()
 
-    def getTotalRecipe( self ) -> dict[str:int]:
+    def __recipesParser(self):
+        for recipe in self.block.recipes:
+            fields = list(recipe.keys())
+            nums = list(recipe.values())
+
+            yield nums[0], fields, nums
+
+    def __isSingleQty(self, r: tuple) -> bool:
+        return True if r[0] == 1 else False
+
+    def getTotalRecipe(self) -> list:
         """计算用户所查询的方块的总配方
 
         Returns:
-            包含配方的字典 -> {'红石粉': 90, ...}
+            包含配方的字典 -> {'outputQty': 10, '红石粉': 90, ...}
         """
-        # 此方法计算 self.block.outputQty != 1 的 block 是不准确的，可能会少不足一组的数量
-        # 而 get_total_to_stacks 方法获取的是准确的
-        total_recipe = { }
-        print(self.block.outputQty)
 
-        if self.block.outputQty == 1:
-            for recipe in self.block.recipes:
-                for k, v in recipe.items():
-                    # @k: 制作 self.block 的配方中的材料名
-                    # @v: 制作 self.block 的配方中材料所需要的数量
-                    total_recipe.update( { k: v * self.qty } )
+        # 当此方法计算产量不等于1的方块时，其计算结果会多出一些.
+        # When this method calculates the number of blocks produced that is not equal to 1,
+        #   -the result it's going to add up a little bit.
 
-        else:
-            pass
-            # for recipe in self.block.recipes:
-            #     for k, v in recipe.items():
-            #         # @k: 制作 self.block 的配方中的材料名
-            #         num = Decimal(
-            #             Decimal( self.qty ) / Decimal( self.block.outputQty )
-            #         ).quantize( custom_exp )
-            #         total = num * v
-            #
-            #         total_recipe.update( { k: int( total ) } )
+        # 但 getTotalToStacks 方法获取的是准确的
+        # But the getTotalToStacks method is accurate
 
-        return total_recipe
+        def getSingleQtyRecipe(parsedRecipe: tuple) -> dict:
+            count = parsedRecipe[0]
+            recipe_fields = parsedRecipe[1]
+            recipe_qty = parsedRecipe[2]
+
+            recipe_fields.insert(0, "count")
+
+            total_nums = [count]
+            for i in recipe_qty:
+                total_nums.append(self.qty * i)
+
+            return dict(zip(recipe_fields, total_nums))
+
+        def getMultiQtyRecipe(parsedRecipe: tuple) -> dict:
+            count = parsedRecipe[0]
+            recipe_fields = parsedRecipe[1]
+            recipe_qty = parsedRecipe[2]
+
+            recipe_fields.insert(0, "count")
+
+            countOfRecipes = Decimal(self.qty / count).quantize(cus_exp)
+
+            multi_total_nums = [count]
+            for i in recipe_qty:
+                multi_total_nums.append(int(i * countOfRecipes))
+
+            return dict(zip(recipe_fields, multi_total_nums))
+
+        result_list = []
+        for i in self.__recipesParser():
+            if self.__isSingleQty(i):
+                result_list.insert(0, getSingleQtyRecipe(i))
+            else:
+                result_list.append(getMultiQtyRecipe(i))
+
+        return result_list
+
+    def getTotalToStacks(self):
+
+        def wrapper(recipe: dict) -> list:
+            data = dict()
+            for k, v in recipe.items():
+                if k in ['count', 'outputQty']:
+                    continue
+                stacks = Decimal(v / self.block.stack).quantize(cus_exp)
+                amount = Decimal(v - stacks * self.block.stack).quantize(cus_exp)
+
+                data.update({k: {"outputQty": recipe['count'], "stacks": int(stacks), "amount": int(amount)}})
+            
+            return data
+
+        def getChange(source: dict):
+            # print( source )
+            # fields = list( source.keys() )
+            # qty = list(source.values())
+            # a = []
+            # if source["count"] != 1:
+            #     pass
+            
+            # for i in qty[2:]:
+            #     # print( i )
+            #     Decimal(  )
+            for block_name, data in source.items():
+                a = Decimal(self.qty - data['stacks'] * data['outputQty'])
+                print( a )
+
+        total_recipes = self.getTotalRecipe()
+        print( total_recipes )
+        for i in total_recipes:
+            print( wrapper(i) )
+            
+            
+        pass
 
 
 if __name__ == "__main__":
     pass
     # main()
-    b = block_factory( '红石中继器' )
-    calc = Calculator( b, 10 )
-    print( calc.getTotalRecipe() )
+    b = block_factory("下界合金锭")
+    calc = Calculator(b, 100)
+    # print(calc.getTotalRecipe())
+    calc.getTotalToStacks()
     # calc.calculate( beautify=True, output_color=Fore.CYAN, inner_output_color=Fore.YELLOW )
--- utils.py
+++ utils.py
@@ -13,10 +13,10 @@ IGNORES = [
     "铁锭", "铜锭", "金锭", "绿宝石", "红石粉", "青金石", "钻石", "煤炭",
     "铁块", "铜块", "金块", "绿宝石块", "红石块", "青金石块", "钻石块", "下届合金块", "煤炭块"
 ]
-STACK_16_BLOCKS = { "盔甲架" }
+STACK_16_BLOCKS = {"盔甲架"}
 
 
-def __utils_getRecipesBookPath( editor: str = "Pycharm" ) -> dict:
+def __utils_getRecipesBookPath(editor: str = "Pycharm") -> dict:
     """
     获取 recipes.json 中引用的具体配方书的路径
     Args:
@@ -33,11 +33,11 @@ def __utils_getRecipesBookPath( editor: str = "Pycharm" ) -> dict:
         "VSCode": "pythonProject/MC Material Calculation/static/recipesBook.json",
     }
 
-    with open( paths[editor], "r", encoding="utf-8" ) as f:
-        return json.load( f )
+    with open(paths[editor], "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
-def __utils_getBlockGroup( groupDict: dict ):
+def __utils_getBlockGroup(groupDict: dict):
     """
     获取方块组的内容
     Args:
@@ -48,13 +48,13 @@ def __utils_getBlockGroup( groupDict: dict ):
         当没有配方书对应的文件 return None.
     """
     for _group_name, group_path in groupDict.items():
-        if not os.path.exists( group_path ):
+        if not os.path.exists(group_path):
             return
-        with open( group_path, "r", encoding="utf-8" ) as f:
-            yield _group_name, json.load( f ).get( _group_name )
+        with open(group_path, "r", encoding="utf-8") as f:
+            yield _group_name, json.load(f).get(_group_name)
 
 
-def __utils_regeditBlocks( _group_name, _group_info: dict[dict] ) -> None:
+def __utils_regeditBlocks(_group_name, _group_info: dict[dict]) -> None:
     """将组内方块的配方注册到 blocks_registry
 
     Args:
@@ -63,11 +63,19 @@ def __utils_regeditBlocks( _group_name, _group_info: dict[dict] ) -> None:
     """
     data = list()
     for k, v in _group_info.items():
-        data.append( k )
-    blocks_registry.update( { _group_name: tuple( data ) } )
+        data.append(k)
+    blocks_registry.update({_group_name: tuple(data)})
 
 
-def utils_checkInRegistry( block_name: str ) -> bool:
+def utils_isIgnore(target_block) -> bool:
+    """判断材料是否在忽略列表内
+    :param target_block: 方块名
+    :return: True: 在忽略列表内 or False: 不在忽略列表内
+    """
+    return target_block in IGNORES
+
+
+def utils_checkInRegistry(block_name: str) -> bool:
     """验证方块是否已注册
 
     Args:
@@ -90,7 +98,7 @@ def utils_checkInRegistry( block_name: str ) -> bool:
         return False
 
 
-def utils_checkStackCount( block_name: str ) -> int:
+def utils_checkStackCount(block_name: str) -> int:
     """检查方块为 16堆叠 还是 64堆叠
 
     Args:
@@ -105,17 +113,8 @@ def utils_checkStackCount( block_name: str ) -> int:
     return STACK_64
 
 
-def utils_checkIgnore( target_block ) -> bool:
-    """判断材料是否在忽略列表内
-    :param target_block: 方块名
-    :return: True: 在忽略列表内 or False: 不在忽略列表内
-    """
-    return target_block in IGNORES
-
-
-blocks_groups = { }
+blocks_groups = {}
 path_dict = __utils_getRecipesBookPath()
-for group_name, group_info in __utils_getBlockGroup( path_dict ):
-    __utils_regeditBlocks( group_name, group_info )
-    blocks_groups.update( { group_name: group_info } )
-
+for group_name, group_info in __utils_getBlockGroup(path_dict):
+    __utils_regeditBlocks(group_name, group_info)
+    blocks_groups.update({group_name: group_info})
